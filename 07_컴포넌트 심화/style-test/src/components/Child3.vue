f555390c0a22df319b2df52903c3a164faebc4d7
jionii <athena020219@naver.com>
2025-03-28 15:32:00 +0900

    scop style-test

---
+++ 07_컴포넌트 심화/style-test/src/components/Child3.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {useCssModule} from 'vue';
+
+const style = useCssModule();
+
+</script>
+
+<template>
+  <div :class="style.child">
+    <h2>Child3</h2>
+  </div>
+</template>
+
+<style module>
+.child {
+    background-color: orangered;
+    border: solid 1px black;
+    margin: 1.5em;
+    padding: 1em;
+}
+</style>
---
+++ 07_컴포넌트 심화/style-test/src/components/Child4.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useCssModule} from 'vue';
+
+const style = useCssModule();
+
+</script>
+
+<template>
+  <div :class="style.child">
+    <h2>Child4</h2>
+  </div>
+</template>
+
+<style module src="@/assets/child4-style.module.css"></style>
