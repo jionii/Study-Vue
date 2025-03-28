e128e5316bef809d561832f0997d4f8e7a643fee
jionii <athena020219@naver.com>
2025-03-28 16:26:33 +0900

    slot 실습

---
+++ 07_컴포넌트 심화/slot-exapmle/src/components/02_Named-slot/Child2.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div>
+    <header>
+      <slot name="header"> </slot>
+    </header>
+
+    <main>
+      <!-- slot name="default" 와 같은 의미. 아무것도 안쓰면 dafault.-->
+      <slot> </slot>
+    </main>
+
+    <footer>
+      <slot name="footer"> </slot>
+    </footer>
+    
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 07_컴포넌트 심화/slot-exapmle/src/components/03_dynamic-slot/Parent3.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Child3 from './Child3.vue';
+import {ref} from 'vue';
+
+const slotName = ref('header');
+
+const changeFooter = () => {
+  slotName.value = 'footer';
+}
+
+const changeDefault = () => {
+  slotName.value = 'default';
+}
+
+const changeHeader = () => {
+  slotName.value = 'header';
+}
+</script>
+
+<template>
+  <!-- 
+  dynamic slot
+  - 부모가 자식 컴포넌트의 슬롯에 동적으로 컨텐츠 할당 가능
+  (슬롯 이름을 동적으로 변경 가능)
+  -->
+  <div style="border: 2px solid blue; padding: 10px;">
+    <Child3 :slot-name="slotName">
+        <template #header>
+          <p>헤더입니다.</p>
+        </template>
+        <template #default> <!-- 설정을 안 해줘도 한 개는 무조건 defalut-->
+          <p>기본슬롯입니다.</p>
+        </template>
+        <template #footer>
+          <p>푸터입니다.</p>
+        </template>
+    </Child3>
+  </div>
+
+  <button @click="changeFooter">슬롯 이름 footer로 변경</button>
+  <button @click="changeDefault">슬롯 이름 default로 변경</button>
+  <button @click="changeHeader">슬롯 이름 default로 변경</button>
+</template>
+
+<style scoped></style>
